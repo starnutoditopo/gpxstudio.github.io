57c4e6a32a2f5d0f3c1130fa8fa0b7ca41b11f3d
Starnuto di topo <starnutoditopo@gmail.com>
2023-11-26 12:23:27 +0100

    bearing markers

--- js/bearingmarkers/leaflet-bearing-markers.js
+++ js/bearingmarkers/leaflet-bearing-markers.js
@@ -4,7 +4,7 @@ L.BearingMarkers = L.LayerGroup.extend({
 		var showAll = Math.min(map.getMaxZoom(), options.showAll || 13);
 		var cssClass = options.cssClass || 'bearing-marker';
 		var textFunction = options.textFunction || function(bearing, i) {
-			return i;
+			return "" + bearing;
 		};
 
 		var zoomLayers = {};
@@ -14,27 +14,17 @@ L.BearingMarkers = L.LayerGroup.extend({
 			coords = line.getLatLngs();
 		}
 		
-		// Get accumulated line lengths as well as overall length
-		var accumulated = L.GeometryUtil.accumulatedLengths(line);
-		var length = accumulated.length > 0 ? accumulated[accumulated.length - 1] : 0;
-		// Position in accumulated line length array
-		//var j = 0;
-		// Number of bearing markers to be added
-		//var count = Math.floor(length / offset);
-
-		alert(coords.length)
-		for (var i = 1; i <= coords.length; ++i) {
-			//var bearing = offset * i;
-			
+		for (var i = 1; i < coords.length; ++i) {
 			var p1 = coords[i - 1];
 			var p2 = coords[i];
-			//var m_line = L.polyline([p1, p2]);
-			//var ratio = (bearing - accumulated[j - 1]) / (accumulated[j] - accumulated[j - 1]);
-			//var position = L.GeometryUtil.interpolateOnLine(map, m_line, ratio);
-			var text = "aaa"; //textFunction.call(this, bearing, i, offset) + Math.round(cumul);
-    		var iconSize = [4+6*text.toString().length,16];
+			var middle = L.latLngBounds(p1, p2).getCenter();
+
+			var bearing = 123;
+
+			var text = textFunction.call(this, bearing, i);
+    		var iconSize = [4+6*text.toString().length, 16];
 			var icon = L.divIcon({ className: cssClass, html: text, iconSize: iconSize });
-			var marker = L.marker(p1, { title: text, icon: icon, pane: 'overlayPane' });
+			var marker = L.marker(middle, { title: text, icon: icon, pane: 'overlayPane' });
 
 			// visible only starting at a specific zoom level
 			var zoom = this._minimumZoomLevelForItem(i, showAll);
@@ -67,7 +57,7 @@ L.BearingMarkers = L.LayerGroup.extend({
 		map.on('zoomend', updateMarkerVisibility);
 
 		this._layers = {}; // need to initialize before adding markers to this LayerGroup
-        this._cumul = cumul + length / 1000;
+        this._cumul = coords.length;
 		updateMarkerVisibility();		
 	},
 
@@ -80,7 +70,6 @@ L.BearingMarkers = L.LayerGroup.extend({
 		}
 		return zoom;
 	},
-
 });
 
 L.Polyline.include({
@@ -120,7 +109,6 @@ L.Polyline.include({
 
 L.LayerGroup.include({
     addBearingMarkers: function(options) {
-		//alert("Add")
         var cumul = 0;
         for (var layer in this._layers) {
             if (typeof this._layers[layer].addBearingMarkers === 'function') {
@@ -131,7 +119,6 @@ L.LayerGroup.include({
         return this;
     },
     removeBearingMarkers: function() {
-		//alert("Remove")
         for (var layer in this._layers) {
             if (typeof this._layers[layer].removeBearingMarkers === 'function') {
                 this._layers[layer].removeBearingMarkers();
